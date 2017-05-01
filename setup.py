d1effc3ebe1946cb97c194ce4875d34674a8ade2
Tobias Schulmann <tobias.schulmann@outlook.com>
2017-05-02 10:08:42 +1200

    Version bump and setup.py changes

--- setup.py
+++ setup.py
@@ -1,11 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name = "pylama_print",
-    version ='0.9.1',
-    author = "Geotob",
-    packages = find_packages(),
-    zip_safe = False,
+    name="pylama_print",
+    version='1.0',
+    author="Tobias Schulmann",
+    author_email="no@ema.il",
+    description="A super-simple pylama linter to flag print() calls",
+    url='https://github.com/GeoTob/pylama_print',
+    packages=find_packages(),
+    zip_safe=False,
     install_requires=[],
     setup_requires=['pytest-runner', 'pylama'],
     tests_require=['pytest', 'pylama'],
